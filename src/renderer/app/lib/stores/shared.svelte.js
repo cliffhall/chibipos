f754e12e268ffaf9a0c1523de459fc7814721ef8
cliffhall <cliff@futurescale.com>
2025-05-10 12:20:05 -0400

    missed file

---
+++ src/renderer/app/lib/stores/shared.svelte.js
@@ -0,0 +1,15 @@
+export const cart = $state([])
+
+export const selectedCat = $state({
+  cat: ''
+})
+
+export const reports = $state({
+  updated: false,
+})
+
+export const printerConfig = $state({
+  visible: false,
+  ip: '192.168.1.2',
+})
+
